33663cad4100b02e512826722c0205f14eceb751
SuaDev09 <francisco.suarez.bastidas@uabc.edu.mx>
2022-07-20 00:34:13 -0700

    Creacion del Model de Comment

---
+++ Models/CommentModel.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace YelpCampAPI.Models;
+
+public class CommentModel 
+{
+    public int Id { get; set;}
+    public int IdUser { get; set;}
+    public string Comment { get; set;}
+
+}
