d5454ef81cf5f97cd78918ae533733f4f37d8b22
SuaDev09 <francisco.suarez.bastidas@uabc.edu.mx>
2022-07-23 04:28:06 -0700

    Controlador creado

---
+++ Controllers/UserController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using YelpCampAPI.Models;
+using YelpCampAPI.Services;
+using YelpCampAPI.Interfaces;
+
+namespace YelpCampAPI.Controllers;
+
+[Route("api/[controller]")]
+
+public class UserController : ControllerBase
+{
+    IUser userService;
+
+    public UserController(IUser service)
+    {
+        userService = service;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(userService.Get());
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] UserModel user)
+    {
+        userService.Save(user);
+        return Ok();
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, [FromBody] UserModel user)
+    {
+        userService.Update(id, user);
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        userService.Delete(id);
+        return Ok();
+    }
+}
